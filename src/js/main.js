8ee5d2999eb0d975971f21d49cbd7b87ae485503
Aleksey Selyavkin <aleksey.selyavkin@gmail.com>
2016-10-26 23:25:55 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 /*
- * Third party
+ * Bower party (from bower_components)
  */
 //= ../../bower_components/jquery/dist/jquery.js
 
 
 /*
- * Custom
+ * Custom (from src/js/partials)
  */
 //= partials/app.js
